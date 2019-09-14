996ff1fabca8d0ec60a3b3be35eaf805b3a12602
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2019-09-14 04:13:28 +0300

    #11 : I fixed the design as possible. Footer ban added.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app',
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
+  @ViewChild('scrollMe', {static: false}) private myScrollContainer: ElementRef;
+
   constructor(){      
   }  
 
-  ngOnInit() {     
+  ngOnInit() { 
+    this.scrollToBottom();    
   } 
+
+  ngAfterViewChecked() {        
+    this.scrollToBottom();        
+  }
+  scrollToBottom(): void {
+      try {
+          this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+      } catch(err) { }                 
+  }
 }
