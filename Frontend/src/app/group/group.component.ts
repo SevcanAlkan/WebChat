de5bd78a3eb28b3ca727441e1a9a9751c1aa6e71
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2019-09-16 00:48:18 +0300

    #9 : Working on CRUD operations for Group

--- Frontend/src/app/group/group.component.ts
+++ Frontend/src/app/group/group.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '@services/GroupService';
+import { Group } from '@models/Group';
 
 @Component({
   selector: 'app-group',
@@ -7,10 +8,24 @@ import { GroupService } from '@services/GroupService';
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent implements OnInit {
+  private group: Group = new Group();
 
-  constructor( private groupService :GroupService,) { }
+  constructor( private groupService :GroupService) {
+      this.group.name = "isim";
+      this.group.description = "aciklama";
+      this.group.isPrivate = true;
+   }
 
   ngOnInit() {
   }
 
+  //Pass id data to here, for edit or delete
+  //If any record dont have id value in save() create new recored. Or update
+  //Set isMain is false on Saving new record and update old record
+  //If old record is already isMain, dont change this value
+  //turncate values before send API
+
+  save(){
+    
+  }
 }
