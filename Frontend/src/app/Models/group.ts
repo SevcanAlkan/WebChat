ab6bb7de40f2345da8cae37d3518e69c553484f8
Sevdzhan Alkan <sevdzhan.alkan@code-runners.com>
2022-05-06 01:42:00 +0300

    Backend solution has been created

--- Frontend/src/app/Models/group.ts
+++
@@ -1,9 +0,0 @@
-import {BaseVM} from '@common/BaseModel';
-
-export class Group extends BaseVM {
-  public name: string;
-  public description: string;
-  public isMain: boolean;
-  public isPrivate: boolean;
-  public users: string[];
-}
--- Frontend/src/app/common/BaseModel.ts
+++
@@ -1,3 +0,0 @@
-export class BaseVM {
-  public id: string;
-}
